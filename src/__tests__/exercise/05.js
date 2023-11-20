258155d295d60266548ca2046d4ea62a2dc52eeb
Staszek Zajaczkowski <staszek.zajaczkowski@gmail.com>
2023-11-20 21:54:10 +0100

    added test error when no password

--- src/__tests__/exercise/05.js
+++ src/__tests__/exercise/05.js
@@ -14,7 +14,7 @@ import { rest } from 'msw';
 import { setupServer } from 'msw/node';
 // 🐨 you'll need to import rest from 'msw' and setupServer from msw/node
 import Login from '../../components/login-submission';
-
+import { handlers } from '../../test/server-handlers';
 const buildLoginForm = build({
   fields: {
     username: fake(f => f.internet.userName()),
@@ -25,12 +25,13 @@ const buildLoginForm = build({
 // 🐨 get the server setup with an async function to handle the login POST request:
 // 💰 here's something to get you started
 const server = setupServer(
-  rest.post(
-    'https://auth-provider.example.com/api/login',
-    async (req, res, context) => {
-      return res(context.json({ username: req.body.username }));
-    },
-  ),
+  // rest.post(
+  //   'https://auth-provider.example.com/api/login',
+  //   async (req, res, context) => {
+  //     return res(context.json({ username: req.body.username }));
+  //   },
+  // ),
+  ...handlers,
 );
 // you'll want to respond with an JSON object that has the username.
 // 📜 https://mswjs.io/
@@ -38,24 +39,35 @@ beforeAll(() => server.listen());
 afterAll(() => server.close());
 // 🐨 before all the tests, start the server with `server.listen()`
 // 🐨 after all the tests, stop the server with `server.close()`
+describe('Login', () => {
+  test(`logging in displays the user's username`, async () => {
+    render(<Login />);
+    const { username, password } = buildLoginForm();
 
-test(`logging in displays the user's username`, async () => {
-  render(<Login />);
-  const { username, password } = buildLoginForm();
+    await userEvent.type(screen.getByLabelText(/username/i), username);
+    await userEvent.type(screen.getByLabelText(/password/i), password);
+    // 🐨 uncomment this and you'll start making the request!
+    await userEvent.click(screen.getByRole('button', { name: /submit/i }));
+    // as soon as the user hits submit, we render a spinner to the screen. That
+    // spinner has an aria-label of "loading" for accessibility purposes, so
+    // 🐨 wait for the loading spinner to be removed using waitForElementToBeRemoved
+    await waitForElementToBeRemoved(() => screen.getByLabelText('loading...'));
+    // screen.debug();
+    // 📜 https://testing-library.com/docs/dom-testing-library/api-async#waitforelementtoberemoved
 
-  await userEvent.type(screen.getByLabelText(/username/i), username);
-  await userEvent.type(screen.getByLabelText(/password/i), password);
-  // 🐨 uncomment this and you'll start making the request!
-  await userEvent.click(screen.getByRole('button', { name: /submit/i }));
-  // as soon as the user hits submit, we render a spinner to the screen. That
-  // spinner has an aria-label of "loading" for accessibility purposes, so
-  // 🐨 wait for the loading spinner to be removed using waitForElementToBeRemoved
-  await waitForElementToBeRemoved(() => screen.getByLabelText('loading...'));
-  screen.debug();
-  // 📜 https://testing-library.com/docs/dom-testing-library/api-async#waitforelementtoberemoved
+    // once the login is successful, then the loading spinner disappears and
+    // we render the username.
+    expect(screen.getByText(username)).toBeInTheDocument();
+    // 🐨 assert that the username is on the screen
+  });
 
-  // once the login is successful, then the loading spinner disappears and
-  // we render the username.
-  expect(screen.getByText(username)).toBeInTheDocument();
-  // 🐨 assert that the username is on the screen
+  test(`logging error message when no password provided`, async () => {
+    render(<Login />);
+    const { username } = buildLoginForm();
+
+    await userEvent.type(screen.getByLabelText(/username/i), username);
+    await userEvent.click(screen.getByRole('button', { name: /submit/i }));
+    await waitForElementToBeRemoved(() => screen.getByLabelText('loading...'));
+    expect(screen.getByRole('alert')).toHaveTextContent('password required');
+  });
 });
