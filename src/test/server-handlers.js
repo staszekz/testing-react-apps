e129226a3559663c4fcf0938c85f1b334219c81c
Staszek Zajaczkowski <staszek.zajaczkowski@gmail.com>
2023-11-25 11:52:58 +0100

    test: 💍 added first snapshop unit test

--- src/test/server-handlers.js
+++ src/test/server-handlers.js
@@ -1,6 +1,6 @@
-import {rest} from 'msw'
+import { rest } from 'msw';
 
-const delay = process.env.NODE_ENV === 'test' ? 0 : 1500
+const delay = process.env.NODE_ENV === 'test' ? 0 : 1500;
 
 const handlers = [
   rest.post(
@@ -10,19 +10,19 @@ const handlers = [
         return res(
           ctx.delay(delay),
           ctx.status(400),
-          ctx.json({message: 'password required'}),
-        )
+          ctx.json({ message: 'password required' }),
+        );
       }
       if (!req.body.username) {
         return res(
           ctx.delay(delay),
           ctx.status(400),
-          ctx.json({message: 'username required'}),
-        )
+          ctx.json({ message: 'username required' }),
+        );
       }
-      return res(ctx.delay(delay), ctx.json({username: req.body.username}))
+      return res(ctx.delay(delay), ctx.json({ username: req.body.username }));
     },
   ),
-]
+];
 
-export {handlers}
+export { handlers };
